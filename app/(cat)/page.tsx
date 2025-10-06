b88c1374859b654a812710d397b0274920ccfacb
LiquidYachting <info@liquid-yachting.com>
2025-10-06 15:27:45 +0200

    Create page.tsx

---
+++ app/(cat)/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/components/Hero";
+
+export default function HomePage() {
+  return (
+    <main>
+      <Hero />
+      {/* Aquí després hi afegirem la resta de seccions com "Recentment venuts", etc. */}
+    </main>
+  );
+}
